4988789a5629ce1baa2e7fc4278bf276c74ec05c
Arsya Khairunissa Budiman <arsya.khairunissa@ui.ac.id>
2026-09-20 07:59:32 +0700

    fix: correct PWS CSRF trusted origin

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -151,4 +151,4 @@ MAILERS = {
     },
 }
 
-CSRF_TRUSTED_ORIGINS = ["https://arsya-khairunissa-myportofolio.pws.cs.ui.ac.id/"]
+CSRF_TRUSTED_ORIGINS = ["https://arsya-khairunissa-myportofolio.pws.cs.ui.ac.id"]
